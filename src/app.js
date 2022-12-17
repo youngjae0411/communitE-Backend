e2562aba5fc7365c4defc51a56ab99ff99cc5883
youngjae0411 <103705842+youngjae0411@users.noreply.github.com>
2022-12-17 10:26:21 +0900

    :sparkles:feat : 게시물 전체 조회 및 생성

--- src/app.js
+++ src/app.js
@@ -3,8 +3,11 @@ const app = express();
 app.use(express.json());
 const cookieParser = require('cookie-parser');
 app.use(cookieParser());
+const routes = require('./routes');
 
 require('dotenv').config();
+
+app.use('/', routes);
 
 app.listen(process.env.PORT, () => {
     console.log(process.env.PORT, '포트로 서버가 열렸습니다.');
@@ -13,4 +16,3 @@ app.listen(process.env.PORT, () => {
 app.get('/', (req, res) => {
     res.send('hello world');
 });
-
