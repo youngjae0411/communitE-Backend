0acf72bfe19ddd2cb5de64b579b92c32de871e17
CHUCHUDACHUCHU <yoon7548@gmail.com>
2022-12-19 21:40:51 +0900

    :fire:fix: user변수 통일

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -49,7 +49,7 @@ class UserController {
     findOneUser = async (req, res) => {
         try {
             const { userId } = req.params;
-            const user = await this.postsService.findOneUser(userId);
+            const user = await this.userService.findOneUser(userId);
             res.status(200).json({ user });
         } catch (error) {
             if (error.message === '존재하지않는 사용자입니다.') {
@@ -57,6 +57,7 @@ class UserController {
                     errorMessage: '존재하지않는 사용자입니다.',
                 });
             }
+            console.log(error);
             res.status(400).json({
                 errorMessage: '사용자정보 불러오기에 실패하였습니다.',
             });
@@ -75,7 +76,7 @@ class UserController {
                 image = 'https://cdn-icons-png.flaticon.com/512/149/149071.png';
             }
 
-            await this.postsService.updateUser(userId, nickname, image);
+            await this.userService.updateUser(userId, nickname, image);
             return res
                 .status(201)
                 .json({ message: '사용자 정보가 수정되었습니다.' });
